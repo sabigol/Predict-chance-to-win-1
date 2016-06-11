30999cec57ec77d820af47a6690ad0a3e7f8051a
Bart Dukes <bartekdukes@gmail.com>
2016-06-11 16:45:35 +0200

    Changed page to cravl

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from domain import *
 from general import *
 
 PROJECT_NAME = 'livescore'
-HOMEPAGE = 'http://www.livescore.in/pl/'
+HOMEPAGE = 'http://www.tablesleague.com/livescores/'
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
 QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
