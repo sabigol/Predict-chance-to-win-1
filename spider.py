8be4f490560dd76d58ac1db088e5e6454afe2e63
Bart Dukes <bartekdukes@gmail.com>
2016-06-11 15:01:58 +0200

    + Generate project + Save links + Set files

---
+++ spider.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from link_finder import LinkFinder
+from general import *
+
+
+class Spider:
+
+    project_name = ''
+
+    def __init__(self):
+        super().__init__()
