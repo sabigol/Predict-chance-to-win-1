29a6f188958185b9986eb40e94a214fd4d98e4dc
Bart Dukes <bartekdukes@gmail.com>
2016-06-11 15:25:31 +0200

    + Added main + Fix for spider

--- domain.py
+++ domain.py
@@ -1,7 +1,7 @@
 from urllib.parse import urlparse
 
 
-# pobieranie normalnej domeny
+# Pobieranie normalnej domeny
 def get_domain_name(url):
     try:
         results = get_sub_domain_name(url).split('.')
@@ -16,5 +16,3 @@ def get_sub_domain_name(url):
         return urlparse(url).netloc
     except:
         return ''
-
-#print(get_domain_name('http://livescore.in/pl/'))
---
+++ main.py
@@ -0,0 +1,15 @@
+import threading
+from queue import Queue
+from spider import Spider
+from domain import *
+from general import *
+
+PROJECT_NAME = 'livescore'
+HOMEPAGE = 'http://www.livescore.in/pl/'
+DOMAIN_NAME = get_domain_name(HOMEPAGE)
+QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+NUMBER_OF_THREADS = 4
+
+queue = Queue()
+Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
